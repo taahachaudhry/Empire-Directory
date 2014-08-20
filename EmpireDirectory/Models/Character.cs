7afe943ad33b29dacd3e2ab9d2522fa055ff9311
taahachaudhry <t44h413@gmail.com>
2014-08-20 10:41:53 -0500

    Jedi class

--- EmpireDirectory/Models/Character.cs
+++ EmpireDirectory/Models/Character.cs
@@ -7,10 +7,13 @@ namespace EmpireDirectory.Models
 {
     public abstract class Character
     {
+        protected static int NextId { get; set; }
         public int ID { get; set; }
         public string Name { get; set; }
         public Affiliation Affiliation { get; set; }
+        public string Bio { get; set; }
         public string Weapon { get; set; }
+        public string Quote { get; set; }
         public string Image { get; set; }
 
     }
---
+++ EmpireDirectory/Models/Jedi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmpireDirectory.Models
+{
+    public class Jedi : Character
+    {        public Rank Rank { get; set; }
+        public string LightSaber { get; set; }
+        public string Force { get; set; }
+        public Jedi(string name, Affiliation affiliation, string bio, string weapon, string quote, string image, Rank rank, string lightsaber, string force)
+        {
+            ID = NextId++;
+            Name = name;
+            Affiliation = affiliation;
+            Bio = bio;
+            Weapon = weapon;
+            Quote = quote;
+            Image = image;
+            Rank = rank;
+            LightSaber = lightsaber;
+            Force = force;
+        }
+    }
+}
