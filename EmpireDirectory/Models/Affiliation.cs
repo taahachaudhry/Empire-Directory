956259407f8a2b8f79c401087808b9d855740277
taahachaudhry <t44h413@gmail.com>
2014-08-20 10:33:08 -0500

    Character Class with Affiliation enum

---
+++ EmpireDirectory/Models/Affiliation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmpireDirectory.Models
+{
+    public enum Affiliation
+    {
+        Empire,
+        Rebel,
+        Mersernary
+    }
+}
---
+++ EmpireDirectory/Models/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmpireDirectory.Models
+{
+    public abstract class Character
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Affiliation Affiliation { get; set; }
+        public string Weapon { get; set; }
+        public string Image { get; set; }
+
+    }
+}
