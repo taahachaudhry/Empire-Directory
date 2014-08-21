dbb54b8a0437364c3424d4cbfeb8bc411fe539d4
taahachaudhry <t44h413@gmail.com>
2014-08-21 09:38:49 -0500

    Singleton

---
+++ EmpireDirectory/Models/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmpireDirectory.Models
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+        List<Character> Characters {get; set;}
+        static Singleton()
+        {
+
+        }
+        private Singleton()
+        {
+
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
